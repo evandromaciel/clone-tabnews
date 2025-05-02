47033ca8cee6d410db8c0c8450018381a48535c7
Evandro Maciel <31519466+evandromaciel@users.noreply.github.com>
2025-05-02 11:56:13 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home() 
-{
-    return <h1>Mãe, amo muito você minha mãezinha Anja!</h1>
+function Home() {
+  return <h1>Mãe, amo muito você minha mãezinha Anja!</h1>;
 }
 
 export default Home;
